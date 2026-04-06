f1a52ba1f2fa7cc6272b160c05a1f1d6050eabae
suber66 <dias.achinbaj@gmail.com>
2026-04-06 23:40:55 +0500

    Dynamic UI Rework
    now UI can have different distance and smoothly moving

--- Scenes/cube_snap_zone.gd
+++ Scenes/cube_snap_zone.gd
@@ -6,5 +6,5 @@ extends XRToolsSnapZone
 
 func _on_has_picked_up(what: Variant) -> void:
 	print("Picked up:",what)
-	#print("Locking")
-	#enabled = false
+	print("Locking")
+	enabled = false
--- Scripts/follow_camera_marker.gd
+++ Scripts/follow_camera_marker.gd
@@ -2,13 +2,28 @@ extends Marker3D
 
 @export var rotation_speed: float = 5.0
 @onready var camera = get_viewport().get_camera_3d()
+@export var ui:XRToolsViewport2DIn3D
+@export var distance: float = -2.0
+@export var scroll_distance: float = 0.2
+@export var min_distance: float = -0.5
+@export var max_distance: float = -4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Signals.PlaySound.connect(_test)
+	Signals.ToggleMusic.connect(_test_2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	distance = clampf(distance,max_distance,min_distance)
+	ui.position.z = lerp(ui.position.z, distance, 5.0 * delta) 
 	global_position = camera.global_position
 	rotation.y = lerp(rotation.y, camera.rotation.y, rotation_speed * delta)
+
+func _test() -> void:
+	distance += scroll_distance
+	
+
+func _test_2() -> void:
+	distance -= scroll_distance
