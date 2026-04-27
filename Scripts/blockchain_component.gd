0cc4aa7fd81f0e66f8855e211e0d0b3fbb2c00c5
suber66 <dias.achinbaj@gmail.com>
2026-04-27 22:04:06 +0500

    added different states for cubes

--- Scripts/blockchain_component.gd
+++ Scripts/blockchain_component.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var cube: XRToolsPickable = %Cube
+@onready var cube_mesh: MeshInstance3D = %MeshInstance3D
 
 @onready var connect_object_anchor: Marker3D = %ConnectObjectAnchor
 @onready var connect_object_snap_zone_anchor: Marker3D = %ConnectObjectSnapZoneAnchor
@@ -11,6 +12,29 @@ extends Node3D
 @export var input_scene: PackedScene
 @export var output_scene: PackedScene
 
+enum state {
+	default,
+	right,
+	wrong
+}
+var current_state: state = state.default : set = _set_state
+
+func _set_state(new_state: state):
+	current_state = new_state
+	match current_state:
+		state.default:
+			color = Color(0,1,1)
+		state.right:
+			color = Color(0,1,0)
+		state.wrong:
+			color = Color(1,0,0)
+
+var color: Color : set = _set_color
+
+func _set_color(new_color: Color):
+	color = new_color
+	cube_mesh.mesh.material.albedo_color = color
+
 var z_range: float = 0.1
 #var held_object:PhysicsBody3D
 var unique_id: String
@@ -20,6 +44,8 @@ var input_objects := []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	unique_id = str(get_instance_id())
+	Signals.PlaySound.connect(_right)
+	Signals.ToggleMusic.connect(_wrong)
 	
 	setup_connectors(input_number, true)
 	setup_connectors(output_number, false)
@@ -27,9 +53,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#if !connect_object.is_picked_up():
-		#connect_object.global_transform = connect_object_anchor.global_transform
-	#connect_object_snap_zone.global_transform = connect_object_snap_zone_anchor.global_transform
 	pass
 
 func setup_connectors(count: int, is_input: bool):
@@ -54,12 +77,8 @@ func setup_connectors(count: int, is_input: bool):
 		instance.position = Vector3(x_pos, 0, z_pos)
 		add_child(instance)
 
+func _right() -> void:
+	current_state = state.right
 
-#func _on_connect_object_snap_zone_has_picked_up(what: Variant) -> void:
-	#held_object = what
-	#held_object.add_collision_exception_with(cube)
-#
-#
-#func _on_connect_object_snap_zone_has_dropped() -> void:
-	#held_object.remove_collision_exception_with(cube)
-	#held_object = null
+func _wrong() -> void:
+	current_state = state.wrong
